d2b7b5b9bde1e845bb433597f27966a65839bfec
susanna <susannafm@gmail.com>
2017-10-01 00:38:57 +0200

    - zynthian_report_saleorder_document --> sale.report_saleorder_document         -- Only shipping address is show         -- Delete Taxes fields         -- Included payment method and their instructions     - zynthian_website_confirmation --> website_sale.confirmation         -- Only Shipping Address is shown and it's move from its original place         -- Delete Taxes Field     - zynthain_sale_order_mail.xml         -- New template for sale order     - zynthian_tracker_sale_order_mail         -- New template for tracking code mail     - zynthian_notification_mail         -- Copy Template "Zynthian Notification Mail" Setting->Technical->Email->Templates to "Notification Email"

--- __openerp__.py
+++ __openerp__.py
@@ -9,18 +9,28 @@
         -- Only shipping address is show
         -- Delete Taxes fields
         -- Included payment method and their instructions
-    -zynthian_website_confirmation --> website_sale.confirmation
+    - zynthian_website_confirmation --> website_sale.confirmation
         -- Only Shipping Address is shown and it's move from its original place
         -- Delete Taxes Field
+    - zynthain_sale_order_mail.xml
+        -- New template for sale order
+    - zynthian_tracker_sale_order_mail
+        -- New template for tracking code mail
+    - zynthian_notification_mail
+        -- Copy Template "Zynthian Notification Mail" Setting->Technical->Email->Templates to "Notification Email"
 
     - IMPORTANT:
         Desactive view report_shop_saleorder_document
         """,
     'author': 'mumaker',
-    'depends': [],
+    'depends': ['mail','report','website_sale'],
     'data': [
         'views/zynthian_report_saleorder_document.xml',
         'views/zynthian_website_confirmation.xml',
+        'views/zynthian_sale_order_mail.xml',
+        'views/zynthian_tracker_sale_order_mail.xml',
+        'views/zynthian_report_layout_header.xml',
+        'views/zynthian_notification_mail.xml',
     ],
     'demo': [],
     'installable': True,
